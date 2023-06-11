10d7001470f119d82018ff59fa2b8dfab75e4102
chunyue <liuchunyueh@gmail.com>
2023-06-11 15:19:29 +0800

    Product Function - refactory category to Eunm type

---
+++ springboot-mall/src/main/java/com/chunyue/springbootmall/constant/ProductCategory.java
@@ -0,0 +1,7 @@
+package com.chunyue.springbootmall.constant;
+
+public enum ProductCategory {
+    FOOD,
+    CAR,
+    BOOK
+}
--- springboot-mall/src/main/java/com/chunyue/springbootmall/rowmapper/ProductRowMapper.java
+++ springboot-mall/src/main/java/com/chunyue/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.chunyue.springbootmall.rowmapper;
 
+import com.chunyue.springbootmall.constant.ProductCategory;
 import com.chunyue.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -13,7 +14,12 @@ public class ProductRowMapper implements RowMapper<Product> {
         Product product = new Product();
 
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+
+        String categoryStr = resultSet.getString("category");
+        ProductCategory productCategory = ProductCategory.valueOf(categoryStr);
+        product.setCategory(productCategory);
+        //product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
+
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
